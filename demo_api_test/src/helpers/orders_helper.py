c1a61b1216cf6347e3420e1813530c5ad685452e
Shameem <shameemakhtar111@gmail.com>
2022-06-04 11:56:18 +0530

    updated code for create_order

--- demo_api_test/src/helpers/orders_helper.py
+++ demo_api_test/src/helpers/orders_helper.py
@@ -1,15 +1,23 @@
 import json
 import os
+from demo_api_test.src.utilities.wooComApiUtilities import WooComAPIUtilities
 
 
 class OrdersHelper:
 
     def __init__(self):
         self.cur_file_dir = os.path.dirname(os.path.realpath(__file__))
+        self.wooApiObj = WooComAPIUtilities()
 
     def create_order(self, additional_args=None):
         payload_template = os.path.join(self.cur_file_dir, '..', 'data', 'create_order_payload.json')
         with open(payload_template) as file:
             payload = json.load(file)
 
-        import pdb;pdb.set_trace()
+        if additional_args:
+            assert isinstance(additional_args, dict), f"Parameter 'additional_args' must be dictionary but found type: {type(additional_args)} "
+            payload.update(additional_args)
+
+        res_api = self.wooApiObj.post('orders', data=payload, expected_status_code=201)
+        return res_api
+
