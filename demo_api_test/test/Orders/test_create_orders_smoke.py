3b792ebdb50c6234d6a4209d9cb705852cf18871
Shameem <shameemakhtar111@gmail.com>
2022-06-04 11:57:07 +0530

    Enhanced test code for tcid48

--- demo_api_test/test/Orders/test_create_orders_smoke.py
+++ demo_api_test/test/Orders/test_create_orders_smoke.py
@@ -1,6 +1,7 @@
-import  pytest
+import pytest
 from demo_api_test.src.dao.products_dao import ProductsDAO
 from demo_api_test.src.helpers.orders_helper import OrdersHelper
+from demo_api_test.src.utilities.wooComApiUtilities import WooComAPIUtilities
 
 
 @pytest.mark.tcid48
@@ -11,9 +12,18 @@ def test_create_paid_order_guest_user():
     # get a product from db
     rand_product = product_dao.get_random_product_from_db(1)
     product_id = rand_product[0]['ID']
+    info = {
+        "line_items": [
+            {
+                "product_id": product_id,
+                "quantity": 1
+            }
+        ]
+    }
 
     # make a call
-    order_obj.create_order()
+    order_json = order_obj.create_order(additional_args=info)
+    import pdb;pdb.set_trace()
 
     # verify response
 
