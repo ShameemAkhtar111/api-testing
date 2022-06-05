cd0ace9daa81a45f9f223c366559e045261d0606
Shameem <shameemakhtar111@gmail.com>
2022-06-05 10:55:08 +0530

    Added code for verifying order details from db

--- demo_api_test/test/Orders/test_create_orders_smoke.py
+++ demo_api_test/test/Orders/test_create_orders_smoke.py
@@ -1,12 +1,15 @@
 import pytest
 from demo_api_test.src.dao.products_dao import ProductsDAO
+from demo_api_test.src.dao.orders_dao import OrdersDao
 from demo_api_test.src.helpers.orders_helper import OrdersHelper
 from demo_api_test.src.utilities.wooComApiUtilities import WooComAPIUtilities
 
+pytestmark = [pytest.mark.orders, pytest.mark.smoke]
 
 @pytest.mark.tcid48
 def test_create_paid_order_guest_user():
     product_dao = ProductsDAO()
+    order_dao = OrdersDao()
     order_obj = OrdersHelper()
 
     # get a product from db
@@ -23,12 +26,21 @@ def test_create_paid_order_guest_user():
 
     # make a call
     order_json = order_obj.create_order(additional_args=info)
+
+    # verify response
     assert order_json, "Create order for guest user response is empty."
     assert order_json['customer_id'] == 0, f"Create order for guest user expected customer_id=0 but got {order_json['customer_id']}"
     assert len(order_json['line_items']) == 1, f"Expected only one item but found {len(order_json['line_items'])}" \
                                                f"for Order id: {order_json['id']}."
-    # import pdb;pdb.set_trace()
-
-    # verify response
-
     # verify db
+
+    line_info = order_dao.get_order_line_by_order_id(order_json['id'])
+    assert line_info, f"Create orders, line item not found in DB. Order id: {order_json['id']}"
+    line_items = [i for i in line_info if i['order_item_type'] == 'line_item']
+    assert len(line_items) == 1, f"Expected 1 line item but got {len(line_items)}. Order id: {order_json['id']}"
+    line_id = line_items[0]['order_item_id']
+    line_details = order_dao.get_order_items_details(line_id)
+    db_product_id = line_details['_product_id']
+
+    assert str(db_product_id) == str(product_id), f"Create order, 'product id' in db does not matche in API." \
+                                        f"API Product id: {product_id}, DB Product id: {product_id}"
