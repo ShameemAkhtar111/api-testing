b8adc580e811777abed63a5711b0e1d625f3a7fa
Shameem <shameemakhtar111@gmail.com>
2022-06-05 10:55:36 +0530

    added method get_order_items_details

--- demo_api_test/src/dao/orders_dao.py
+++ demo_api_test/src/dao/orders_dao.py
@@ -8,3 +8,11 @@ class OrdersDao:
     def get_order_line_by_order_id(self,order_id):
         sql= f"SELECT * FROM demosite.wp_woocommerce_order_items WHERE order_id={order_id};"
         return self.db_utility.execute_select(sql)
+
+    def get_order_items_details(self, order_item_id):
+        sql = f"SELECT * FROM demosite.wp_woocommerce_order_itemmeta WHERE order_item_id={order_item_id};"
+        rs_sql = self.db_utility.execute_select(sql)
+        line_details = dict()
+        for meta in rs_sql:
+            line_details[meta['meta_key']] = meta['meta_value']
+        return line_details
