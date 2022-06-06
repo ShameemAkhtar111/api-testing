d6edd33d4f60b5080c861c54ab108813550556d5
Shameem <shameemakhtar111@gmail.com>
2022-06-06 11:44:55 +0530

    Added setup method 'order_setup' using pytest fixture

--- demo_api_test/test/Orders/test_create_orders_smoke.py
+++ demo_api_test/test/Orders/test_create_orders_smoke.py
@@ -5,17 +5,28 @@ from demo_api_test.src.helpers.orders_helper import OrdersHelper
 from demo_api_test.src.helpers.customers_helper import CustomerHelper
 from demo_api_test.src.utilities.wooComApiUtilities import WooComAPIUtilities
 
-
 pytestmark = [pytest.mark.orders, pytest.mark.smoke]
 
-@pytest.mark.tcid48
-def test_create_paid_order_guest_user():
+@pytest.fixture(scope='module')
+def orders_setup():
     product_dao = ProductsDAO()
-    order_obj = OrdersHelper()
-
-    # get a product from db
+    order_helper = OrdersHelper()
     rand_product = product_dao.get_random_product_from_db(1)
     product_id = rand_product[0]['ID']
+
+    info = {'product_id': product_id,
+            'order_helper': order_helper}
+
+    return info
+
+
+@pytest.mark.tcid48
+def test_create_paid_order_guest_user(orders_setup):
+
+    # get a product from db
+    order_helper = orders_setup['order_helper']
+    product_id = orders_setup['product_id']
+    import pdb;pdb.set_trace()
     customer_id = 0
     info = {
         "line_items": [
@@ -28,22 +39,22 @@ def test_create_paid_order_guest_user():
     }
 
     # make a call
-    order_json = order_obj.create_order(additional_args=info)
+    order_json = order_helper.create_order(additional_args=info)
 
     # verify response
     expected_products = [{'product_id': product_id}]
-    order_obj.verfiy_order_is_created(order_json, customer_id, expected_products)
+    order_helper.verfiy_order_is_created(order_json, customer_id, expected_products)
 
 
 
 @pytest.mark.tcid49
-def test_create_paid_order_new_customer():
-    product_dao = ProductsDAO()
-    order_obj = OrdersHelper()
+def test_create_paid_order_new_customer(orders_setup):
+
     customer_helper = CustomerHelper()
 
-    rand_product = product_dao.get_random_product_from_db(1)
-    product_id = rand_product[0]['ID']
+    order_helper = orders_setup['order_helper']
+    product_id = orders_setup['product_id']
+    import pdb;pdb.set_trace()
     cust_info = customer_helper.create_customer()
     customer_id = cust_info['id']
     info = {
@@ -57,7 +68,7 @@ def test_create_paid_order_new_customer():
     }
 
     # make a call
-    order_json = order_obj.create_order(additional_args=info)
+    order_json = order_helper.create_order(additional_args=info)
 
     expected_products = [{'product_id': product_id}]
-    order_obj.verfiy_order_is_created(order_json, customer_id, expected_products)
+    order_helper.verfiy_order_is_created(order_json, customer_id, expected_products)
