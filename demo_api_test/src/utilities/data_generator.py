01626c9945ac518c6d7cc4ed81b3971a57988112
Shameem <shameemakhtar111@gmail.com>
2022-11-23 17:48:06 +0530

    upadated data_generator

--- demo_api_test/src/utilities/data_generator.py
+++ demo_api_test/src/utilities/data_generator.py
@@ -1,12 +1,14 @@
+import os
+import pandas as pd
 from datetime import date
 from dateutil.relativedelta import relativedelta
-
 from faker import Faker
 
 class DataGenerator:
     __fake = None
     __person = {}
     __company = {}
+    __cc_details = {}
     def __init__(self,langLocale):
         self.__fake=Faker(locale=langLocale)
         domain=self.__fake.domain_word()
@@ -114,43 +116,108 @@ class DataGenerator:
     def get_phone_number(self):
         return self.__person.get('phone_number')
 
-    def get_full_credit_card(self,cardType=None):
+    def get_full_credit_card(self, cardType = None):
+        card_details = None
         if cardType is not None:
             if cardType in ['maestro', 'mastercard', 'visa16', 'visa13', 'visa19', 'amex', 'discover', 'diners', 'jcb15', 'jcb16','master','visa']:
-                self.__person['credit_card'] = self.__fake.credit_card_full(cardType)
+                card_details = self.__fake.credit_card_full(cardType)
             else:
                 raise Exception("OPPS!!, please enter a valid card type i.e from"
                                 "'maestro', 'mastercard', 'visa16', 'visa13', 'visa19', 'amex', 'discover',"
                                 " 'diners', 'jcb15', 'jcb16','master','visa'")
         else:
-            self.__person['credit_card'] = self.__fake.credit_card_full()
+            # self.__person['credit_card'] = self.__fake.credit_card_full()
+            card_details = self.__fake.credit_card_full().split("\n")
+        cc_num_exp = card_details[2].split(" ")
+        cc_cvv = card_details[3].split(": ")
+        self.__cc_details['credit_card_type'] = card_details[0]
+        self.__cc_details['credit_card_number'] = cc_num_exp[0]
+        self.__cc_details['credit_card_exp_date'] = cc_num_exp[1]
+        self.__cc_details['credit_card_cvv'] = cc_cvv[1]
 
-        return self.__person.get('credit_card')
+        self.__person['credit_card'] = self.__cc_details
+
+        # return self.__person.get('credit_card')
 
     def get_credit_card_number(self,cardType=None):
-        return self.__fake.credit_card_number(cardType)
+        if len(self.__cc_details) == 0:
+            if cardType is not None:
+                self.get_full_credit_card(cardType)
+            else:
+                self.get_full_credit_card()
+        return self.__person.get('credit_card').get('credit_card_number')
 
     def get_credit_card_provider(self):
-        return self.__fake.credit_card_provider()
-
-    # def get_name_on_credit_card(self,cardType=None):
-    #     # return self.__fake.credit_card_provider()
-    #     return self.__person.get('credit_card').get('name_on_card')
+        if len(self.__cc_details) == 0:
+            self.get_full_credit_card()
+        return self.__person.get('credit_card').get('credit_card_type')
 
     def get_credit_card_exp_date(self):
-        return self.__fake.credit_card_expire()
+        if len(self.__cc_details) == 0:
+            self.get_full_credit_card()
+        return self.__person.get('credit_card').get('credit_card_exp_date')
 
     def get_credit_card_cvv_num(self,cardType=None):
-        return self.__fake.credit_card_security_code(cardType)
+        if len(self.__cc_details) == 0:
+            if cardType is not None:
+                self.get_full_credit_card(cardType)
+            else:
+                self.get_full_credit_card()
+        return self.__person.get('credit_card').get('credit_card_cvv')
 
 
-# dg = DataGenerator('en-IN')
-# dg.get_person()
-# print(dg.person)
-dg2 = DataGenerator('en-IN')
-print(dg2.get_full_credit_card())
-print(dg2.get_credit_card_number())
-print(dg2.get_credit_card_provider())
-# print(dg2.get_name_on_credit_card())
-print(dg2.get_credit_card_exp_date())
-print(dg2.get_credit_card_cvv_num())
+if __name__ == "__main__":
+    files_list = os.listdir()
+    excel_file = None
+    for file in files_list:
+        if '.xlsx' in file:
+            excel_file = file
+            break
+    print(os.path.abspath(excel_file))
+    df = pd.read_excel(os.path.abspath(excel_file), engine="openpyxl")
+    df.columns = df.columns.str.lower()
+    df.fillna("nan", inplace = True)
+    dict_vals = df.to_dict()
+    local_val= dict_vals.get('locale')[0]
+    no_of_records = int(dict_vals.get('# of data')[0])
+    headers = list(dict_vals.get('headers').values())
+    frmat = dict_vals.get('format').values()
+    constraint = dict_vals.get('constraints').values()
+    dm = zip(headers, frmat, constraint)
+    print(list(dm))
+    print(local_val, no_of_records)
+    header_dictionary = {}
+    for x in headers:
+        header_dictionary[x] = []
+
+    for key in header_dictionary.keys():
+        x = []
+        for i in range(1, no_of_records+1):
+            dg = DataGenerator('en-IN')
+            if key.lower() == 'sno':
+                x.append(i)
+            elif key.lower() == "firstname":
+                x.append(dg.get_first_name())
+            elif key.lower() == "lastname":
+                x.append(dg.get_last_name())
+            elif key.lower() == "address" or 'address' in key.lower():
+                x.append(dg.get_full_address())
+            elif key.lower() == "age":
+                x.append(dg.get_age())
+            elif key.lower() == "dob" or key.lower() == "dateofbirth":
+                x.append(dg.get_birthdate())
+            elif key.lower() == "creditcardnumber" or key.lower() == "ccnumber":
+                x.append(dg.get_credit_card_number())
+            elif key.lower() == "creditcardexpirydate" or key.lower() == "ccexpirydate" or key.lower() == "expirydate":
+                x.append(dg.get_credit_card_exp_date())
+        header_dictionary[key] = x
+    print(header_dictionary)
+    df2 = pd.DataFrame(header_dictionary)
+    writer = pd.ExcelWriter("test_data2.xlsx", engine='xlsxwriter')
+    df2.to_excel(writer, sheet_name="Sheet1", index=False)
+    writer.save()
+
+
+
+
+
